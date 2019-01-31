ee62c9b11c20ba31127e3a4a4ba573d9536ac6b8
arunsurya77 <arunsuryaoffice@gmail.com>
2019-01-31 14:40:00 -0800

    Working on array passing

--- cy_quicksort.pyx
+++ cy_quicksort.pyx
@@ -2,8 +2,9 @@ cimport quicksort
 import numpy as np
 cimport numpy as np
 
-def quicksort_c(int[:,:,:] arr):
-    cdef int[::1, :,:] arr_f = arr.copy_fortran()
-    cdef int[::1, :,:] results = <int[::1, :, :]> quicksort.quick_sort(&arr_f[0,0,0], arr_f.shape[0], arr_f.shape[1], arr_f.shape[2])
+def quicksort_c(int[:,:,:] arr_f):
+    #cdef int[::1, :,:] arr_f = arr.copy_fortran()
+    #nelem=arr_f.shape[0]*arr_f.shape[1]*arr_f.shape[2]
+    cdef int[:,:,:] results = <int[:arr_f.shape[0],:arr_f.shape[1],:arr_f.shape[2]]> quicksort.quick_sort(&arr_f[0,0,0], arr_f.shape[0], arr_f.shape[1], arr_f.shape[2])
     return np.asarray(results)
     
--- quicksort.c
+++ quicksort.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int* quick_sort (int* arr, int xlim, int ylim, int zlim ) {
-    int i, j, k;
-    int l=0;
+    int i, j, k,nelem;
+    int* new_arr;
+    nelem=xlim*ylim*zlim;
+    new_arr = (int *) malloc(nelem* sizeof(int));
+    int l=0,m=0;
     for (i = 0; i<xlim; i++) {
 		for (j = 0; j<ylim; j++) {
 			for (k = 0; k<zlim; k++) {
 				printf("%d",arr[l]);
+				new_arr[l]=arr[l];
 				l++;
 			}
+			m++;
 		}
+		
 	}
 	return arr;
 
